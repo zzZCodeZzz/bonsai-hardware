1916f9438b5c6ebc08ea6988645f494741bbbd4d
manuelzach <zachmanu@informatik.hu-berlin.de>
2022-05-14 23:04:07 +0200

    v2

--- src/irrigation/irrigation.module.ts
+++ src/irrigation/irrigation.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { IrrigationService } from './irrigation.service';
+import { PumpService } from '../pumps/pump.service';
+import { SoilMoistureService } from '../sensors/soil-moisture/soil-moisture.service';
 
 @Module({
-  providers: [IrrigationService],
+  providers: [IrrigationService, PumpService, SoilMoistureService],
 })
 export class IrrigationModule {}
