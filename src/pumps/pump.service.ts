0eb8f61590b55a218899ccbebcb11b551bd70d65
manuelzach <zachmanu@informatik.hu-berlin.de>
2022-05-14 23:21:51 +0200

    v2

--- src/pumps/pump.service.ts
+++ src/pumps/pump.service.ts
@@ -4,6 +4,7 @@ import { Gpio } from 'onoff';
 @Injectable()
 export class PumpService {
   public turnPump(state: 'on' | 'off') {
+    console.log('pump', state);
     const pump = new Gpio(17, 'out');
     pump.writeSync(state === 'on' ? 1 : 0);
   }
