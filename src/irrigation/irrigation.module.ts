04891b5059a38ae03280a0d4cf718f0776cbf580
manuelzach <zachmanu@informatik.hu-berlin.de>
2022-05-15 02:08:25 +0200

    v2

--- src/irrigation/irrigation.module.ts
+++ src/irrigation/irrigation.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { IrrigationService } from './irrigation.service';
 import { PumpService } from '../pumps/pump.service';
 import { SoilMoistureService } from '../sensors/soil-moisture/soil-moisture.service';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Module({
-  providers: [IrrigationService, PumpService, SoilMoistureService],
+  providers: [
+    IrrigationService,
+    PumpService,
+    SoilMoistureService,
+    FirebaseService,
+  ],
 })
 export class IrrigationModule {}
--- src/irrigation/irrigation.service.ts
+++ src/irrigation/irrigation.service.ts
@@ -2,20 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { SoilMoistureService } from '../sensors/soil-moisture/soil-moisture.service';
 import { PumpService } from '../pumps/pump.service';
 import { delay } from '../utils';
+import { Cron } from '@nestjs/schedule';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Injectable()
 export class IrrigationService {
   constructor(
     private soilMoistureService: SoilMoistureService,
     private pumpService: PumpService,
+    private firebaseService: FirebaseService,
   ) {}
 
   // At every 30th minute.
-  /* @Cron('*!/30 * * * *')*/
+  @Cron('*/30 * * * *')
   public async irrigate() {
     const soilMoisture = await this.soilMoistureService.getPercentage();
 
-    if (soilMoisture < 45) {
+    await this.firebaseService.safeToFirestore('soilMoisture', {
+      percentage: soilMoisture,
+    });
+
+    if (soilMoisture < 38.5) {
       this.pumpService.turnPump('on');
       await delay(3000);
       this.pumpService.turnPump('off');
