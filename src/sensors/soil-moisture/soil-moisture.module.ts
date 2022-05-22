141a8080298988650513966324d2f1cc8f2e916f
manuelzach <zachmanu@informatik.hu-berlin.de>
2022-05-22 15:15:57 +0200

    SoilMoistureController put in module

--- src/sensors/soil-moisture/soil-moisture.module.ts
+++ src/sensors/soil-moisture/soil-moisture.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FirebaseService } from '../../firebase/firebase.service';
 import { SoilMoistureService } from './soil-moisture.service';
+import { SoilMoistureController } from './soil-moisture.controller';
 
 @Module({
   providers: [SoilMoistureService, FirebaseService],
   exports: [SoilMoistureService],
+  controllers: [SoilMoistureController],
 })
 export class SoilMoistureModule {}
