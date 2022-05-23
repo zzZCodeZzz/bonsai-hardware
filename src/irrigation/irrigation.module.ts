f576355e5e1523c907282bb48da98e51655cae52
manuelzach <zachmanu@informatik.hu-berlin.de>
2022-05-23 23:23:05 +0200

    trying to send mqtt

--- src/irrigation/irrigation.module.ts
+++ src/irrigation/irrigation.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { IrrigationService } from './irrigation.service';
 import { PumpService } from '../pumps/pump.service';
-import { SoilMoistureService } from '../sensors/soil-moisture/soil-moisture.service';
 import { FirebaseService } from '../firebase/firebase.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { SoilMoistureModule } from '../sensors/soil-moisture/soil-moisture.module';
 
 @Module({
   imports: [
+    SoilMoistureModule,
     ClientsModule.register([
       {
         name: 'MQTT_SERVICE',
@@ -17,12 +18,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       },
     ]),
   ],
-  providers: [
-    IrrigationService,
-    PumpService,
-    SoilMoistureService,
-    FirebaseService,
-  ],
+  providers: [IrrigationService, PumpService, FirebaseService],
   exports: [IrrigationService],
 })
 export class IrrigationModule {}
