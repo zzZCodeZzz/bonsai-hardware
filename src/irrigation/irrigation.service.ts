d6d75f89b3e25464d726fd5456a73df053a7e027
manuelzach <zachmanu@informatik.hu-berlin.de>
2022-05-24 23:16:35 +0200

    * percentage based on ringbuffer * connected to nodemcu via mqtt

--- src/irrigation/irrigation.service.ts
+++ src/irrigation/irrigation.service.ts
@@ -15,37 +15,11 @@ export class IrrigationService {
   ) {}
 
   // At every 30th minute.
-  /* @Cron('*!/30 * * * *')
+  @Cron('*/30 * * * *')
   public async irrigate() {
-    const soilMoisture = await this.soilMoistureService.getPercentage();
-
-    const timestamp = Timestamp.now();
-
-    if (soilMoisture < 81) {
-      this.pumpService.turnPump('on');
-      await delay(3000);
-      this.pumpService.turnPump('off');
-      await delay(3000);
-      const moistAfterWatering = await this.soilMoistureService.getPercentage();
-      await this.firebaseService.safeToFirestore('soilMoisture', {
-        percentage: moistAfterWatering,
-        timestamp,
-      });
-    } else {
-      await this.firebaseService.safeToFirestore('soilMoisture', {
-        percentage: soilMoisture,
-        timestamp,
-      });
-    }
-  }*/
-
-  // At every 30th minute.
-  @Cron('*/1 * * * *')
-  public async newIrrigate() {
     const percentage =
       this.soilMoistureService.getPercentageFromRingBuffer('zitronen-melisse');
 
-    console.log('percentage', percentage);
     if (percentage < 81) {
       await this.client.emit('/pump/zitronen-melisse', {
         mode: 'start',
